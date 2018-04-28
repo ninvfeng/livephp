0257e17eccb8ff2dbc36a1d325c9651589526680
冯誉信 <ninvfeng@qq.com>
2018-04-28 11:12:52 +0800

    swoole 添加启动,停止,重启命令

--- swoole/http_server.php
+++ swoole/http_server.php
@@ -21,25 +21,48 @@ header("Access-Control-Allow-Origin: *");
 //根据命名空间自动加载php文件
 spl_autoload_register('App::autoload');
 
-$http = new swoole_http_server(config('swoole_http_server.host'),config('swoole_http_server.port'));
+$cmd=$_SERVER['argv'][1]?$_SERVER['argv'][1]:'start';
 
-$http->on("start", function ($server) {
-    echo "Swoole http server is started at http://".config('swoole_http_server.host').":".config('swoole_http_server.port')."\n";
-});
+if($cmd=='start'){
+	start();
+}elseif($cmd=='stop'){
+	stop();
+}elseif($cmd=='restart'){
+	stop();
+	sleep(1);
+	start();
+}
 
-$http->on("request", function ($request, $response) {
-	if($request->server['request_uri']=='/favicon.ico'){
-		$response->header('Content-Type', 'image/x-icon');
-		$response->sendfile(__DIR__.$request->server['request_uri']);
-	}else{
-	    $_GET = $request->get;
-	    $_POST = $request->post;
-	    $_COOKIE = $request->cookie;
-	    $_FILES = $request->files;
-	    $_SERVER = $request->server;
-	    App::run()->dispatch();
-	    $response->end(app()->res);		
+//启动服务
+function start(){
+	$http = new swoole_http_server(config('swoole_http_server.host'),config('swoole_http_server.port'));
+	$http->set(config('swoole_http_server'));
+	$http->on("start", function ($server) {
+	    echo "Swoole http server is started\n";
+	});
+
+	$http->on("request", function ($request, $response) {
+		if($request->server['request_uri']=='/favicon.ico'){
+			$response->header('Content-Type', 'image/x-icon');
+			$response->sendfile(__DIR__.$request->server['request_uri']);
+		}else{
+		    $_GET = $request->get;
+		    $_POST = $request->post;
+		    $_COOKIE = $request->cookie;
+		    $_FILES = $request->files;
+		    $_SERVER = $request->server;
+		    App::run()->dispatch();
+		    $response->end(app()->res);		
+		}
+	});
+
+	$http->start();
+}
+
+//关闭服务
+function stop(){
+	$pid=@file_get_contents(config('swoole_http_server.pid_file'));
+	if($pid){
+		exec('kill '.$pid);
 	}
-});
-
-$http->start();
+}
