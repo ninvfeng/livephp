ffd20c084352a685bf27c86ef50884e88d5bc5f5
冯誉信 <ninvfeng@qq.com>
2018-06-05 10:37:36 +0800

    更新数据库操作类

--- core/library/Mysql.php
+++ core/library/Mysql.php
@@ -10,6 +10,7 @@ class Mysql
     protected $_limit='';
     protected $_join='';
     protected $_debug=false;
+    protected $_param=[];
 
     function __construct($config=[])
     {
@@ -22,6 +23,9 @@ class Mysql
 
         //设置客户端字符集
         $this->_pdo->exec("set names 'utf8'");
+
+        //禁用prepared statements的仿真效果 确保SQL语句和相应的值在传递到mysql服务器之前是不会被PHP解析
+        $this->_pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
 
         //数据表
         $this->_table=$table;
@@ -61,7 +65,14 @@ class Mysql
         if(is_array($where)){
             $res='';
             foreach($where as $k => $v){
-                $res.='`'.$k.'`="'.$v.'" and';
+                $column_key='';
+                foreach (explode('.',$k) as $kk => $vv) {
+                    $column_key.='`'.$vv.'`.';
+                    $column_plac='where_'.$vv;
+                }
+                $this->_param[$column_plac]=$v;
+                $column_key=trim($column_key,'.');
+                $res.=$column_key.'=:'.$column_plac.' and';
             }
             $where=trim($res,'and');
         }
@@ -80,6 +91,11 @@ class Mysql
 
     //join
     public function join($join){
+
+        //语句中不包含join时自动添加left join
+        if(stripos($join,'join')===false){
+            $join='left join '.$join;
+        }
         $this->_join=$join;
         return $this;
     }
@@ -92,7 +108,15 @@ class Mysql
 
     //结果集
     public function select(){
-        return $this->_query();
+        $res=$this->_query();
+        if(count($res[0])==1){
+            $column=explode('.',$this->_field);
+            $column=array_pop($column);
+            $result=array_column($res,$column);
+            return $result;
+        }else{
+            return $res;
+        }
     }
 
     //获取单条数据
@@ -105,27 +129,40 @@ class Mysql
         if($this->_where){
             $update='';
             foreach($data as $k => $v){
-                $update.="`".$k."`='".$v."',";
+                $column_key='';
+                foreach (explode('.',$k) as $kk => $vv) {
+                    $column_key.='`'.$vv.'`.';
+                    $column_plac=$vv;
+                }
+                $this->_param[$column_plac]=$v;
+                $column_key=trim($column_key,'.');
+                $update.=$column_key."=:".$column_plac.",";
             }
             $update=trim($update,',');
             $sql="update {$this->_table} set $update {$this->_where};";
-            return $this->_exec($sql);
+            return $this->exec($sql,$this->_param);
         }else{
-            error('保存数据需指定条件');
+            echo '保存数据需指定条件';
+            die();
         }
-
-
     }
 
     //添加
     public function insert($data){
         $update='';
         foreach($data as $k => $v){
-            $update.="`".$k."`='".$v."',";
+            $column_key='';
+            foreach (explode('.',$k) as $kk => $vv) {
+                $column_key.='`'.$vv.'`.';
+                $column_plac=$vv;
+            }
+            $this->_param[$column_plac]=$v;
+            $column_key=trim($column_key,'.');
+            $update.=$column_key."=:".$column_plac.",";
         }
         $update=trim($update,',');
         $sql="insert into {$this->_table} set $update;";
-        $this->_exec($sql);
+        $this->exec($sql,$this->_param);
         return $this->_pdo->lastInsertId();
     }
 
@@ -133,33 +170,44 @@ class Mysql
     public function delete(){
         if($this->_where){
             $sql="delete from {$this->_table} {$this->_where};";
-            return $this->_exec($sql);
+            return $this->exec($sql,$this->_param);
         }else{
-            error('删除数据需指定条件');
+            echo '删除数据需指定条件';
+            die();
         }
     }
 
     //执行原生query
     public function query($sql,$param=[]){
-        //禁用prepared statements的仿真效果 确保SQL语句和相应的值在传递到mysql服务器之前是不会被PHP解析
-        $this->_pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
-
-        $pre=$this->_pdo->prepare($sql);
-        $pre->execute($param);
-        if($this->_error()){
-            return $pre->fetchAll(\PDO::FETCH_ASSOC);
+        if($this->_debug){
+            echo "<pre>";
+            echo $sql;
+            echo "<br>";
+            print_r($param);
+            die();
+        }else{
+            $pre=$this->_pdo->prepare($sql);
+            $pre->execute($param);
+            if($this->_error()){
+                return $pre->fetchAll(\PDO::FETCH_ASSOC);
+            }
         }
     }
 
     //执行原生exec
     public function exec($sql,$param=[]){
-        //禁用prepared statements的仿真效果 确保SQL语句和相应的值在传递到mysql服务器之前是不会被PHP解析
-        $this->_pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
-
-        $pre=$this->_pdo->prepare($sql);
-        $res=$pre->execute($param);
-        if($this->_error()){
-            return $res;
+        if($this->_debug){
+            echo "<pre>";
+            echo $sql;
+            echo "<br>";
+            print_r($param);
+            die();
+        }else{
+            $pre=$this->_pdo->prepare($sql);
+            $res=$pre->execute($param);
+            if($this->_error()){
+                return $res;
+            }
         }
     }
 
@@ -180,31 +228,10 @@ class Mysql
         }
     }
 
-    //执行
-    protected function _exec($sql){
-        if($this->_debug){
-            echo $sql;
-            die();
-        }else{
-            $res=$this->_pdo->exec($sql);
-            if($this->_error()){
-                return $res;
-            }
-        }
-    }
-
     //查询
     protected function _query(){
         $sql="select {$this->_field} from {$this->_table} {$this->_join} {$this->_where} {$this->_order} {$this->_limit}";
-        if($this->_debug){
-            echo $sql;
-            die();
-        }else{
-            $res=$this->_pdo->query($sql);
-            if($this->_error()){
-                return $res->fetchAll(\PDO::FETCH_ASSOC);
-            }
-        }
+        return $this->query($sql,$this->_param);
     }
 
     //错误处理
